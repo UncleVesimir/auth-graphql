62fc25bb146f3f28c7ddf06d886eadc490014706
UncleVesimir <jmzmelling@gmail.com>
2017-11-05 15:52:03 +0000

    Add Dashboard Component and route

--- client/components/App.js
+++ client/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header'
 import LoginForm from './LoginForm'
 import SignUpForm from './SignUpForm'
 import Landing from './Landing';
+import Dashboard from './Dashboard';
 
 const App = (props) =>{
 
@@ -16,6 +17,7 @@ const App = (props) =>{
         <Route exact path="/" component={Landing} />
         <Route path={match.url + "login"} component={LoginForm}/>
         <Route path={match.url + "signup"} component={SignUpForm}/>
+        <Route path={match.url + "dashboard"} component={Dashboard}/>
       </Switch>
     </div>
   )
---
+++ client/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = (props) => {
+  return (
+    <div>You are logged in, Welcome to the dashboard!</div>
+  )
+};
+
+export default Dashboard;
